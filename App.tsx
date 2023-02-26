02c6aa851bb60be1effa19b1c859c0f80b228847
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2023-02-26 10:38:10 +0500

    horizontal list in home screen

--- App.tsx
+++ App.tsx
@@ -3,21 +3,20 @@ import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
-  Text,
   View,
   useColorScheme,
+  ScrollView,
   StyleSheet,
-  Image,
+  Text,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-import {XColors} from './src/config/constants';
-
-import {Accented, Bold} from './src/components/formatting.component';
 import {Header} from './src/components/header.component';
+import {RestaurantCard} from './src/components/restaurant-card.component';
+import {Accented} from './src/components/formatting.component';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -35,91 +34,25 @@ function App(): JSX.Element {
       <NavigationContainer>
         <View style={styles.screen}>
           <Header />
-          <View style={styles.body}>
-            <View
-              style={{
-                padding: 20,
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <Text style={{fontSize: 24, fontWeight: '500'}}>
-                Specially Selected For You
-              </Text>
-              <Accented>
-                <AntDesign name="arrowright" size={24} />
-              </Accented>
-            </View>
-            <View
-              style={{
-                elevation: 2,
-                backgroundColor: 'white',
-                width: 280,
-                borderRadius: 5,
-                overflow: 'hidden',
-                margin: 5,
-              }}>
-              <View
-                style={{
-                  position: 'relative',
-                  display: 'flex',
-                  height: 180,
-                }}>
-                <View style={StyleSheet.absoluteFill}>
-                  <Image
-                    style={{width: '100%'}}
-                    source={require('./assets/download.jpeg')}
-                  />
-                </View>
-                <View
-                  style={{
-                    ...styles.iconButton,
-                    opacity: 0.8,
-                    alignSelf: 'flex-end',
-                    margin: 10,
-                  }}>
-                  <AntDesign name="hearto" size={16} color="tomato" />
-                </View>
-                <View
-                  style={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: 'white',
-                    borderRadius: 20,
-                    paddingHorizontal: 15,
-                    paddingVertical: 10,
-                    alignSelf: 'flex-end',
-                    marginRight: 20,
-                    position: 'absolute',
-                    top: 150,
-                    right: -10,
-                    elevation: 5,
-                  }}>
-                  <Bold>
-                    <Text>20 - 30</Text>
-                  </Bold>
-                  <Text>min</Text>
-                </View>
-              </View>
-              <View style={{padding: 10}}>
-                <Text style={{fontSize: 20, fontWeight: '500'}}>
-                  FRENCY TACOS - 1
-                </Text>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <Accented>
-                    <AntDesign name="star" size={14} />
-                  </Accented>
-                  <Text>
-                    <Accented>
-                      <Text>4.1 Rating</Text>
-                    </Accented>{' '}
-                    (500+)
-                  </Text>
-                </View>
-              </View>
-            </View>
+          <View
+            style={{
+              padding: 20,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontSize: 24, fontWeight: '500'}}>
+              Specially Selected For You
+            </Text>
+            <Accented>
+              <AntDesign name="arrowright" size={24} />
+            </Accented>
           </View>
+          <ScrollView horizontal>
+            <RestaurantCard />
+            <RestaurantCard />
+          </ScrollView>
         </View>
       </NavigationContainer>
     </SafeAreaView>
@@ -129,14 +62,6 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   screen: {},
   body: {},
-  iconButton: {
-    width: 40,
-    height: 40,
-    backgroundColor: XColors.lightgrey,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 export default App;
---
+++ src/components/restaurant-card.component.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {XColors} from '../config/constants';
+
+import {Accented, Bold} from './formatting.component';
+
+export const RestaurantCard = props => {
+  return (
+    <View>
+      <View
+        style={{
+          elevation: 2,
+          backgroundColor: 'white',
+          width: 280,
+          borderRadius: 5,
+          overflow: 'hidden',
+          margin: 5,
+        }}>
+        <View
+          style={{
+            position: 'relative',
+            display: 'flex',
+            height: 180,
+          }}>
+          <View style={StyleSheet.absoluteFill}>
+            <Image
+              style={{width: '100%'}}
+              source={require('./../../assets/download.jpeg')}
+            />
+          </View>
+          <View
+            style={{
+              ...styles.iconButton,
+              opacity: 0.8,
+              alignSelf: 'flex-end',
+              margin: 10,
+            }}>
+            <AntDesign name="hearto" size={16} color="tomato" />
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'white',
+              borderRadius: 20,
+              paddingHorizontal: 15,
+              paddingVertical: 10,
+              alignSelf: 'flex-end',
+              marginRight: 20,
+              position: 'absolute',
+              top: 150,
+              right: -10,
+              elevation: 5,
+            }}>
+            <Bold>
+              <Text>20 - 30</Text>
+            </Bold>
+            <Text>min</Text>
+          </View>
+        </View>
+        <View style={{padding: 10}}>
+          <Text style={{fontSize: 20, fontWeight: '500'}}>
+            FRENCY TACOS - 1
+          </Text>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Accented>
+              <AntDesign name="star" size={14} />
+            </Accented>
+            <Text>
+              <Accented>
+                <Text>4.1 Rating</Text>
+              </Accented>{' '}
+              (500+)
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  iconButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: XColors.lightgrey,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
